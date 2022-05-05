874558967155adf102e9da0e3275e253848c99fd
AlexBalex <104983739+AlexBalex@users.noreply.github.com>
2022-05-05 14:05:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+ifstream fin("read.txt");
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout <<'\n';
+}
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void Selection_Sort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+void Bubble_Sort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)
+        for (j = 0; j < n-i-1; j++)
+            if (arr[j] > arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+}
+void Insertion_Sort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+void Quick_Sort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int pi = partition(arr, low, high);
+
+
+        Quick_Sort(arr, low, pi - 1);
+        Quick_Sort(arr, pi + 1, high);
+    }
+
+}
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+
+        heapify(arr, n, largest);
+    }
+}
+void Heap_Sort(int arr[], int n)
+{
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+
+    for (int i = n - 1; i > 0; i--)
+    {
+
+        swap(arr[0], arr[i]);
+
+        heapify(arr, i, 0);
+    }
+}
+void merge(int array[], int const left, int const mid, int const right)
+{
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+
+    auto *leftArray = new int[subArrayOne],
+            *rightArray = new int[subArrayTwo];
+
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,
+            indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+void Merge_Sort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+    Merge_Sort(array, begin, mid);
+    Merge_Sort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+void countsort(int arr[], int n, int exp)
+{
+    int output[n];
+    int i, count[10] = { 0 };
+
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+void Radix_Sort(int arr[], int n)
+{
+
+    int m = getMax(arr, n);
+
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countsort(arr, n, exp);
+}
+void Count_Sort(int array[], int size) {
+
+    int output[500001];
+    int count[500001];
+    int max = array[0];
+
+
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max)
+            max = array[i];
+    }
+
+    for (int i = 0; i <= max; ++i) {
+        count[i] = 0;
+    }
+
+
+    for (int i = 0; i < size; i++) {
+        count[array[i]]++;
+    }
+
+    for (int i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        output[count[array[i]] - 1] = array[i];
+        count[array[i]]--;
+    }
+    for (int i = 0; i < size; i++) {
+        array[i] = output[i];
+    }
+}
+
+int main()
+{
+    int i=0,n=0,a[500001],b[500001];
+    float sum=0.0;
+    while(fin>>a[i])
+    {
+        n++;
+        i++;
+    }
+    std::cout<<i<<'\n';
+    /*
+    Selection_Sort(b,n);
+    Bubble_Sort(b,n);
+    Insertion_Sort(b,n);
+    Heap_Sort(b,n);
+    Quick_Sort(b,0,n-1);
+    Merge_Sort(b,0,n-1);
+    Radix_Sort(b,n);
+    Count_Sort(b,n);
+    */
+    for(int k=0;k<=n;k++)
+        b[k]=a[k];
+    for(int j=1;j<=100;j++)
+    {
+        for(int k=0;k<=n;k++)
+            b[k]=a[k];
+        auto begin = std::chrono::high_resolution_clock::now();
+        //printArray(b,n);
+        Count_Sort(b,n);
+        //printArray(b,n);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+        sum=elapsed.count() * 1e-9+sum;
+        printf("Time measured: %.9f seconds.\n", elapsed.count() * 1e-9);
+    }
+    printf("Final Time: %.9f seconds.\n", sum/100);
+    for(int k=0;k<=n;k++)
+        a[k]=b[k];
+
+    //printArray(a,n);
+    fin.close();
+    return 0;
+
+}
